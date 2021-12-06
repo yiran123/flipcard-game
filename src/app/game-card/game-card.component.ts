b7fd84da61181a467823da7b288c463b9d2f02fd
yiran123 <yyyiran@hotmail.com>
2021-12-06 00:19:25 -0800

    picture update

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -1,20 +1,51 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { CardData } from '../CardData.interface';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { CardData } from "../CardData.interface";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-game-card',
-  templateUrl: './game-card.component.html',
-  styleUrls: ['./game-card.component.css']
+  selector: "app-game-card",
+  templateUrl: "./game-card.component.html",
+  styleUrls: ["./game-card.component.css"],
+  animations: [
+    trigger("cardFlip", [
+      state(
+        "default",
+        style({
+          transform: "none",
+        })
+      ),
+      state(
+        "flipped",
+        style({
+          transform: "perspective(600px) rotateY(180deg)",
+        })
+      ),
+      state(
+        "matched",
+        style({
+          visibility: "true",
+          transform: "perspective(600px) rotateY(180deg)",
+          // opacity: 0,
+        })
+      ),
+      transition("default => flipped", [animate("400ms")]),
+      transition("flipped => default", [animate("400ms")]),
+      transition("* => matched", [animate("400ms")]),
+    ]),
+  ],
 })
 export class GameCardComponent implements OnInit {
-
   @Input() data: CardData;
 
   @Output() cardClicked = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
